1064727fbb03312197767ec6e688d4f0a7dd43a2
tinnikov <tinnikovkr@gmail.com>
2022-04-04 22:22:27 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+    public void accordion() {
+        System.out.println("Петя играет на баяне");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.song();
+        petya.song();
+        petya.song();
+        petya.accordion();
+        petya.accordion();
+        petya.accordion();
+    }
+}
