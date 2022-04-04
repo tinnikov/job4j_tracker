802edbb85d704cfa7cc342afd45e622ec0f8f09e
tinnikov <tinnikovkr@gmail.com>
2022-04-04 22:25:58 +0300

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -6,7 +6,7 @@ public class Student {
     }
 
     public void accordion() {
-        System.out.println("Петя играет на баяне");
+         System.out.println("Петя играет на баяне");
     }
 
     public static void main(String[] args) {
