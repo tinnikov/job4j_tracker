417aa8f0a8b9df396006b2679755ca4a084614ee
tinnikov <tinnikovkr@gmail.com>
2022-04-06 23:14:24 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/oop/MaxTest.java
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void whenAmoreB() {
+    public void when5more2() {
         Max max2 = new Max();
         int expected = 5;
         int result = max2.max(5, 2);
@@ -16,7 +16,7 @@ public class MaxTest {
     }
 
     @Test
-    public void WhenBmoreThenAc() {
+    public void When11MoreThan4and7() {
         Max max3 = new Max();
         int expected = 11;
         int result = max3.max(4, 11, 7);
@@ -24,7 +24,7 @@ public class MaxTest {
     }
 
     @Test
-    public void WhenCmoreThenAbC() {
+    public void When21MoreThan4and11adn15() {
         Max max4 = new Max();
         int expected = 21;
         int result = max4.max(4, 11, 21, 15);
