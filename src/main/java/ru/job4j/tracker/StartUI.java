7b5e0021850a94ed972261076ea356c427099af3
tinnikov <tinnikovkr@gmail.com>
2022-04-10 17:20:02 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,12 +4,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         Item dataFormat = new Item();
         String formatData = dataFormat.getCreated().format(formatter);
         System.out.println(formatData);
     }
-
-
 }
