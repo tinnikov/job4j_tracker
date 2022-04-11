05b2b67d06dcddb184e857fc26d0dab4191d7e4e
tinnikov <tinnikovkr@gmail.com>
2022-04-11 20:34:24 +0300

    4. Переопределение [#174890 #287036]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,7 @@ public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
-        return "{" + ln + "\t\"name\" :" + " " + "\"" + name + "\"," + ln + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
-    }
+        return "{" + ln + "\t\"name\" :" + " " + "\"" + name + "\","
+                + ln + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
+                }
 }
