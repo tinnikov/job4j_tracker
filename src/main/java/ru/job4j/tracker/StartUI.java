17149de6fc51267aeb73df029d62f62427380646
tinnikov <tinnikovkr@gmail.com>
2022-04-18 22:31:15 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -19,6 +19,17 @@ public class StartUI {
             } else if (select == 6) {
                 run = false;
             }
+            else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+            }
+            }
         }
     }
 
