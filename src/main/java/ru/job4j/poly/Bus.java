b68f508ffc1a056666789c109212824d24387431
tinnikov <tinnikovkr@gmail.com>
2022-04-18 23:38:11 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+        System.out.println("Поехали");
+    }
+
+    @Override
+    public void askPass(int passenger) {
+        System.out.println("Количество пассажиров: " + passenger);
+    }
+
+    @Override
+    public int price(int refuel) {
+        return refuel * 49;
+    }
+}
