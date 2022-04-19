f6b3af6f57a6e02d07d8b04290420199a6c5bd1d
tinnikov <tinnikovkr@gmail.com>
2022-04-19 22:29:47 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778]

--- src/main/java/ru/job4j/tracker/Input.java
+++ src/main/java/ru/job4j/tracker/Input.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker;
 
 public interface Input {
-    String askStr(String question);
+     String askStr(String question);
 
-    int askInt(String question);
+     int askInt(String question);
 }
