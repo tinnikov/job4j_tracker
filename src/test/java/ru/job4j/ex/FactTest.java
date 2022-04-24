6ea4ce54cfd647b634112769c438b7f372b3e70c
tinnikov <tinnikovkr@gmail.com>
2022-04-24 18:38:38 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenNnegative() {
