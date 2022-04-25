2a45548ea81e278eb5f0c734d8c784d6cd7aa178
tinnikov <tinnikovkr@gmail.com>
2022-04-25 22:17:17 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+        String[] test = new String[] {"Moscow", "Krasnodar", "Abakan"};
+        System.out.println(indexOf(test, "Krasnodar"));
+        }
+        catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
