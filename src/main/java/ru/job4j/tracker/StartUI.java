c94d2196157bfab8e8b9404064a89e675c58f3f9
tinnikov <tinnikovkr@gmail.com>
2022-04-30 13:18:35 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -30,6 +30,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+
         Output output = new ConsoleOutput();
         Input input = new ValidateInput();
         Tracker tracker = new Tracker();
