a68a01346d15f3222e35de2eedccb84def5350ab
tinnikov <tinnikovkr@gmail.com>
2022-05-09 10:28:21 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,18 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+            final Airbus airbus = new Airbus("A320");
+            airbus.printModel();
+            airbus.printCountEngine();
+
+            airbus.setName("A380");
+            airbus.printModel();
+            airbus.printCountEngine();
+
+            airbus.setName("IL160");
+            airbus.printModel();
+            airbus.printCountEngine();
+
+    }
+}
