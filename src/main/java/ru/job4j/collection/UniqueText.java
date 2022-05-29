214398039b76c7f57db509aa90a7a46815724fb3
tinnikov <tinnikovkr@gmail.com>
2022-05-29 11:18:04 +0300

    2. Идентичные тексты [#212655 #305653]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
+
         boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
