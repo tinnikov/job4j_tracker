c6998c26ba5a6e7130919a6ca73a569e48b8bbcf
tinnikov <tinnikovkr@gmail.com>
2022-08-17 11:48:53 +0300

    srt

--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
-
+import java.util.*;
 import java.util.HashSet;
-import java.util.List;
 
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
