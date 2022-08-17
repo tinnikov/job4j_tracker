e33cd6935dea145c5b0b129a0a01badd4299105e
tinnikov <tinnikovkr@gmail.com>
2022-08-17 11:09:09 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,72 @@
+package ru.job4j.early;
+
+import java.util.Locale;
+
+public class PasswordValidator {
+    public static String validate(String password) {
+
+        boolean upperFlag = false;
+        boolean lowFlag = false;
+        boolean isDigit = false;
+        boolean isSpecCh = false;
+        boolean testStr = true;
+
+        String[] noStr = new String[] {"qwerty", "12345", "password", "admin", "user"};
+
+        if (password == null) {
+            throw new IllegalArgumentException("Пароль не может быть null");
+        }
+
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Пароль должен быть в диапазоне от 8 до 32 символов");
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+
+            if (Character.isUpperCase(password.charAt(i))) {
+                upperFlag = true;
+            }
+
+            if (Character.isLowerCase(password.charAt(i))) {
+                lowFlag = true;
+            }
+            if (Character.isDigit(password.charAt(i))) {
+                isDigit = true;
+            }
+
+           if (!Character.isLetterOrDigit(password.charAt(i))) {
+                isSpecCh = true;
+            }
+            if (upperFlag && lowFlag && isDigit && isSpecCh) {
+                break;
+            }
+        }
+        for (String s : noStr) {
+            String low = s.toLowerCase();
+            if (password.contains(low)) {
+                testStr = false;
+                break;
+            }
+        }
+
+        if (!upperFlag) {
+            throw new IllegalArgumentException("Пароль должен содержать хоть одну заглавную букву");
+        }
+        if (!lowFlag) {
+            throw new IllegalArgumentException("Пароль должен содержать хоть одну строчную букву");
+        }
+        if (!isDigit) {
+            throw new IllegalArgumentException("Пароль должен содержать хотя бы одну цифру");
+        }
+
+        if (!isSpecCh) {
+            throw new IllegalArgumentException("Пароль должен содержать один спецсимвол");
+        }
+        if (!testStr) {
+            throw new IllegalArgumentException("Пароль не должен содержать подстроки в любых регистрах: qwerty, 12345, password, admin, user");
+        }
+        String rsl = "Валидация пройдена";
+        return rsl;
+    }
+
+}
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.early;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    public void whenValidPass() {
+        String password = "Tinnikovkr1!";
+        String expected = PasswordValidator.validate(password);
+        assertThat(expected).isEqualTo("Валидация пройдена");
+    }
+
+    @Test
+    public void whenPassIsNull() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate(null);
+        });
+        Assertions.assertEquals("Пароль не может быть null", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsDiapozon() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("Passwor");
+        });
+        Assertions.assertEquals("Пароль должен быть в диапазоне от 8 до 32 символов", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsOneUpperLetter() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("password1");
+        });
+        Assertions.assertEquals("Пароль должен содержать хоть одну заглавную букву", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsOneLowLetter() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("PASSWORD1");
+        });
+        Assertions.assertEquals("Пароль должен содержать хоть одну строчную букву", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsOneDigit() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("Password");
+        });
+        Assertions.assertEquals("Пароль должен содержать хотя бы одну цифру", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsOneSpecSymb() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("Password1");
+        });
+        Assertions.assertEquals("Пароль должен содержать один спецсимвол", thrown.getMessage());
+    }
+
+    @Test
+    public void whenPassIsNoSubstring() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, ()-> {
+            PasswordValidator.validate("password123P?");
+        });
+        Assertions.assertEquals("Пароль не должен содержать подстроки в любых регистрах: qwerty, 12345, password, admin, user", thrown.getMessage());
+    }
+}
