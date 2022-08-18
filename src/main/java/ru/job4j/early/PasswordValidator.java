b5c16d6ba7a81c4792589a0a1bed90fb30ff9154
tinnikov <tinnikovkr@gmail.com>
2022-08-18 10:29:48 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #333264]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,39 +1,29 @@
 package ru.job4j.early;
 
-import java.util.Locale;
-
 public class PasswordValidator {
     public static String validate(String password) {
-
         boolean upperFlag = false;
         boolean lowFlag = false;
         boolean isDigit = false;
         boolean isSpecCh = false;
         boolean testStr = true;
-
         String[] noStr = new String[] {"qwerty", "12345", "password", "admin", "user"};
-
         if (password == null) {
             throw new IllegalArgumentException("Пароль не может быть null");
         }
-
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Пароль должен быть в диапазоне от 8 до 32 символов");
         }
-
         for (int i = 0; i < password.length(); i++) {
-
             if (Character.isUpperCase(password.charAt(i))) {
                 upperFlag = true;
             }
-
             if (Character.isLowerCase(password.charAt(i))) {
                 lowFlag = true;
             }
             if (Character.isDigit(password.charAt(i))) {
                 isDigit = true;
             }
-
            if (!Character.isLetterOrDigit(password.charAt(i))) {
                 isSpecCh = true;
             }
@@ -48,7 +38,6 @@ public class PasswordValidator {
                 break;
             }
         }
-
         if (!upperFlag) {
             throw new IllegalArgumentException("Пароль должен содержать хоть одну заглавную букву");
         }
@@ -58,15 +47,13 @@ public class PasswordValidator {
         if (!isDigit) {
             throw new IllegalArgumentException("Пароль должен содержать хотя бы одну цифру");
         }
-
         if (!isSpecCh) {
             throw new IllegalArgumentException("Пароль должен содержать один спецсимвол");
         }
         if (!testStr) {
-            throw new IllegalArgumentException("Пароль не должен содержать подстроки в любых регистрах: qwerty, 12345, password, admin, user");
+            throw new IllegalArgumentException("Пароль не должен содержать подстроки в любых регистрах: "
+                    + "qwerty, 12345, password, admin, user");
         }
-        String rsl = "Валидация пройдена";
-        return rsl;
+        return "Валидация пройдена";
     }
-
 }
